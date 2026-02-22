9350433e8a09298b6188d1dec610de7954ddb33d
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-02-22 21:54:56 +0100

    added current nvim config

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,5 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
