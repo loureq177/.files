6c42cbda224c0a2b08ef0b31deee4ec4e9fb68b9
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-04-23 15:24:47 +0200

    preference update

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,13 +1,8 @@
--- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
-
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
 vim.opt.scrolloff = 8
-
 vim.opt.mousescroll = "ver:1,hor:1"
 vim.opt.cmdheight = 0
-
--- Use system clipboard for yank/paste (requires wl-clipboard on Wayland)
 vim.opt.clipboard = "unnamedplus"
--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,6 @@ return {
 	{ "folke/flash.nvim", enabled = false },
 	{ "kristijanhusak/vim-dadbod-ui", enabled = false },
 	{ "MagicDuck/grug-far.nvim", enabled = false },
+	{ "tpope/vim-dadbod", enabled = false },
+	{ "kristijanhusak/vim-dadbod-completion", enabled = false },
 }
