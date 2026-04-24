7856cd20ac4363cf8e27f05e17632564fcc4a88b
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-04-24 18:53:47 +0200

    preference update

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,4 @@
 return {
-	{ "catppuccin/nvim", enabled = false },
 	{ "folke/flash.nvim", enabled = false },
 	{ "kristijanhusak/vim-dadbod-ui", enabled = false },
 	{ "MagicDuck/grug-far.nvim", enabled = false },
