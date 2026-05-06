3141a7b122843dde2cc96617929d2d868da9221c
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-05-06 12:04:29 +0200

    feat(nvim): enhance text editing and add DiffClipboard command

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,9 +2,22 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "markdown",
+	pattern = { "markdown", "text" },
 	callback = function()
 		vim.opt_local.spell = true
 		vim.opt_local.spelllang = { "en", "pl" }
+		vim.opt_local.textwidth = 80
+		vim.opt_local.formatoptions:append("t")
+		vim.opt_local.wrap = true
+		vim.opt_local.linebreak = true
 	end,
 })
+
+vim.api.nvim_create_user_command("DiffClipboard", function()
+	local ftype = vim.bo.filetype
+	vim.cmd("vsplit | enew")
+	vim.bo.filetype = ftype
+	vim.bo.buftype = "nofile"
+	vim.cmd('normal! "+P')
+	vim.cmd("windo diffthis")
+end, {})
