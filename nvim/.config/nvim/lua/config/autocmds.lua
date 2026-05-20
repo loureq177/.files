0ce996b8f881f09d94be8214e1d01ff159e38fd5
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-05-20 10:09:20 +0200

    nvim: fixed problems with LSP for md and txt

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -4,10 +4,9 @@
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "markdown", "text" },
 	callback = function()
-		vim.opt_local.spell = true
-		vim.opt_local.spelllang = { "en", "pl" }
-		vim.opt_local.textwidth = 75
-		vim.opt_local.formatoptions:add("t")
+		vim.opt_local.spell = false
+		vim.opt_local.textwidth = 0
+		vim.opt_local.formatoptions:remove("t")
 		vim.opt_local.wrap = true
 		vim.opt_local.linebreak = true
 	end,
