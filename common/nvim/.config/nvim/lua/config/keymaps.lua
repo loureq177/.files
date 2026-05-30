80b3c33cb366b787bb3c0458ed52d0a2445685ee
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-05-31 00:31:09 +0200

    nvim: added clipboard and black hole register keymaps

--- common/nvim/.config/nvim/lua/config/keymaps.lua
+++ common/nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,14 @@
 -- Quit with Ctrl+q
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-q>", "<cmd>qa<cr>", { desc = "Quit all" })
 
+-- Yanks (copies) to system clipboard using <leader>y
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+
+-- Changes and deletes (cuts) without replacing system clipboard contents
+vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete to black hole register" })
+vim.keymap.set({ "n", "v" }, "D", '"_D', { desc = "Delete to end of line (black hole)" })
+vim.keymap.set({ "n", "v" }, "c", '"_c', { desc = "Change to black hole register" })
+vim.keymap.set({ "n", "v" }, "C", '"_C', { desc = "Change to end of line (black hole)" })
+
+-- Optional: If you still want to "cut" to the system clipboard, explicitly use another key combo
+vim.keymap.set({ "n", "v" }, "<leader>d", '"+d', { desc = "Delete to system clipboard" })
