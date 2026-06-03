a709a85fd99514a16c6f3ee67b0c117cf3834537
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-06-03 22:48:07 +0200

    hypr: added pinch gestures and cleaned up input config

--- archlinux/hypr/.config/hypr/conf/input.lua
+++ archlinux/hypr/.config/hypr/conf/input.lua
@@ -1,7 +1,3 @@
----------------
----- INPUT ----
----------------
-
 hl.config({
 	input = {
 		kb_layout = "pl",
@@ -9,14 +5,10 @@ hl.config({
 		kb_model = "",
 		kb_options = "caps:escape,altwin:swap_lalt_lwin",
 		kb_rules = "",
-
 		repeat_delay = 200,
 		repeat_rate = 25,
-
 		follow_mouse = 1,
-
 		sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
-
 		touchpad = {
 			natural_scroll = true,
 			tap_to_click = true,
@@ -24,15 +16,13 @@ hl.config({
 	},
 })
 
-hl.gesture({
-	fingers = 3,
-	direction = "horizontal",
-	action = "workspace",
-})
+hl.gesture({ fingers = 3, direction = "horizontal", scale = 2.0, action = "workspace" })
+hl.gesture({ fingers = 2, direction = "pinchin", action = "cursorZoom", zoom_level = 2.0, mode = "mult" })
+hl.gesture({ fingers = 2, direction = "pinchout", action = "cursorZoom", zoom_level = 0.5, mode = "mult" })
 
 -- Example per-device config
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
-hl.device({
-	name = "epic-mouse-v1",
-	sensitivity = -0.5,
-})
+-- hl.device({
+-- 	name = "epic-mouse-v1",
+-- 	sensitivity = -0.5,
+-- })
