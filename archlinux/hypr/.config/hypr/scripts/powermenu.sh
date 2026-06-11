eda7785f38636e3695dc931bab1d141dc0892c70
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-06-11 21:25:01 +0200

    hypr: fix powermenu shutdown entry label

--- archlinux/hypr/.config/hypr/scripts/powermenu.sh
+++ archlinux/hypr/.config/hypr/scripts/powermenu.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-entry=$(printf " lock\n logout\n suspend\n reboot\n shutdown" | rofi -dmenu -p 'Power' -format s)
+entry=$(printf " lock\n logout\n suspend\n reboot\n poweroff" | rofi -dmenu -p 'Power' -format s)
 
 case "$entry" in
 " lock") hyprlock ;;
 " logout") hyprctl dispatch exit ;;
 " suspend") systemctl suspend ;;
 " reboot") hyprshutdown --post-cmd "systemctl reboot" ;;
-" shutdown") hyprshutdown --post-cmd "systemctl poweroff" ;;
+" poweroff") hyprshutdown --post-cmd "systemctl poweroff" ;;
 esac
