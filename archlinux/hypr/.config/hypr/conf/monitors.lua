6fea4e84ddb5a520b0d2b2b5af471b2f40fa4b86
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-06-20 17:59:39 +0200

    hypr: scale laptop display dynamically on monitor changes

--- archlinux/hypr/.config/hypr/conf/monitors.lua
+++ archlinux/hypr/.config/hypr/conf/monitors.lua
@@ -1,12 +1,22 @@
-hl.monitor({
-	output = "desc:BOE 0x0998",
-	mode = "1920x1080@165",
-	position = "0x0",
-	scale = "1.2",
-})
+local function update_laptop_scale()
+	local monitors = hl.get_monitors()
+
+	hl.monitor({
+		output = "desc:BOE 0x0998",
+		mode = "1920x1080@165",
+		position = "0x0",
+		scale = (#monitors > 1) and 1.2 or 1,
+	})
+end
+
 hl.monitor({
 	output = "",
 	mode = "preferred",
 	position = "auto",
 	scale = "auto",
 })
+
+hl.on("monitor.added", update_laptop_scale)
+hl.on("monitor.removed", update_laptop_scale)
+
+update_laptop_scale()
