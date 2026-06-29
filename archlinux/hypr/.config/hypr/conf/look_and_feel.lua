e4629e59b2a558c8faf2929df175ebdd5bbef615
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-06-29 22:16:22 +0200

    hypr: update inactive border to tokyonight

--- archlinux/hypr/.config/hypr/conf/look_and_feel.lua
+++ archlinux/hypr/.config/hypr/conf/look_and_feel.lua
@@ -8,7 +8,7 @@ hl.config({
 				colors = { "rgba(33ccffee)", "rgba(00ff99ee)" },
 				angle = 45,
 			},
-			inactive_border = "rgba(595959aa)",
+			inactive_border = "rgba(292e42ee)",
 		},
 		resize_on_border = true,
 		allow_tearing = false,
