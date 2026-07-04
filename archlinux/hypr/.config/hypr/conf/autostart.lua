38049ff340f95566d6e70ee1b5ace665e3a43520
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-07-04 16:49:06 +0200

    hypr: reduce gtk font sizes

--- archlinux/hypr/.config/hypr/conf/autostart.lua
+++ archlinux/hypr/.config/hypr/conf/autostart.lua
@@ -20,10 +20,10 @@ hl.on("hyprland.start", function()
 		"systemctl --user import-environment " .. env_vars,
 		gsettings .. " cursor-theme 'Bibata-Modern-Classic'",
 		gsettings .. " icon-theme 'Papirus-Dark'",
-		gsettings .. " font-name 'Adwaita Sans 13'",
+		gsettings .. " font-name 'Adwaita Sans 12'",
 		gsettings .. " color-scheme 'prefer-dark'",
 		gsettings .. " gtk-theme 'Adwaita-dark'",
-		gsettings .. " monospace-font-name 'JetBrainsMono Nerd Font Mono 13'",
+		gsettings .. " monospace-font-name 'JetBrainsMono Nerd Font Mono 12'",
 		gsettings .. " text-scaling-factor 1.1",
 		"systemctl --user start graphical-session.target",
 		"systemctl --user start xdg-desktop-portal-hyprland "
