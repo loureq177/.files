8561567fd79d7fe5b6fe8754c6e61984d93a5fcf
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-07-12 22:52:32 +0200

    nvim: restructure formatting config and add markdown prettier

--- common/nvim/.config/nvim/lua/plugins/formatting.lua
+++ common/nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,21 +1,19 @@
 return {
-	"stevearc/conform.nvim",
-	opts = function(_, opts)
-		opts.formatters_by_ft = opts.formatters_by_ft or {}
-		opts.formatters_by_ft.php = { "php_cs_fixer" }
+	{
+		"stevearc/conform.nvim",
+		opts = function(_, opts)
+			opts.formatters_by_ft = opts.formatters_by_ft or {}
+			opts.formatters_by_ft.php = { "php_cs_fixer" }
+			opts.formatters_by_ft.markdown = { "prettier" }
 
-		opts.formatters = opts.formatters or {}
-		opts.formatters.php_cs_fixer = {
-			args = {
-				"fix",
-				"$FILENAME",
-				"--no-interaction",
-			},
-		}
-
-		opts.format_on_save = {
-			timeout_ms = 1500,
-			lsp_fallback = true,
-		}
-	end,
+			opts.formatters = opts.formatters or {}
+			opts.formatters.php_cs_fixer = {
+				args = {
+					"fix",
+					"$FILENAME",
+					"--no-interaction",
+				},
+			}
+		end,
+	},
 }
