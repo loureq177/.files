a21a82fa44324c597b854da7543f130533c8350f
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-07-12 22:52:32 +0200

    hypr: drop DISPLAY/WAYLAND env vars and remove & backgrounding from autostart

--- archlinux/hypr/.config/hypr/conf/autostart.lua
+++ archlinux/hypr/.config/hypr/conf/autostart.lua
@@ -1,8 +1,6 @@
 local gsettings = "gsettings set org.gnome.desktop.interface"
+
 local env_vars = table.concat({
-	"DISPLAY",
-	"WAYLAND_DISPLAY",
-	"XDG_CURRENT_DESKTOP",
 	"XDG_SESSION_TYPE",
 	"XDG_SESSION_DESKTOP",
 	"GTK_THEME",
@@ -18,23 +16,26 @@ hl.on("hyprland.start", function()
 	local cmds = {
 		"dbus-update-activation-environment --systemd " .. env_vars,
 		"systemctl --user import-environment " .. env_vars,
+
 		gsettings .. " cursor-theme 'Bibata-Modern-Classic'",
 		gsettings .. " icon-theme 'Papirus-Dark'",
 		gsettings .. " font-name 'Adwaita Sans 13'",
 		gsettings .. " color-scheme 'prefer-dark'",
 		gsettings .. " gtk-theme 'Adwaita-dark'",
 		gsettings .. " monospace-font-name 'JetBrainsMono Nerd Font Mono 13'",
-		"systemctl --user start graphical-session.target",
-		"systemctl --user start xdg-desktop-portal-hyprland "
-			.. "xdg-desktop-portal-gtk xdg-desktop-portal",
-		"wl-paste --type text --watch cliphist -max-items 50 store &",
-		"wl-paste --type image/png --watch cliphist -max-items 10 store &",
+
+		"systemctl --user start hyprland-session.target",
+
+		"systemctl --user start xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal",
+
+		"wl-paste --type text --watch cliphist -max-items 50 store",
+		"wl-paste --type image/png --watch cliphist -max-items 10 store",
 		"wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 0.25",
-		"waybar &",
-		"swaybg -i ~/.config/hypr/wallpapers/hyprland.png &",
-		"swaync &",
-		"hypridle &",
-		"hyprsunset &",
+		"waybar",
+		"swaybg -i ~/.config/hypr/wallpapers/hyprland.png",
+		"swaync",
+		"hypridle",
+		"hyprsunset",
 	}
 
 	for _, cmd in ipairs(cmds) do
