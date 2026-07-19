45bb3349d6c6ead1c1986784a69130c7e891ded2
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-07-19 22:36:13 +0200

    nvim: increased performance

--- common/nvim/.config/nvim/lua/plugins/disabled.lua
+++ common/nvim/.config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,6 @@ return {
 	{ "kristijanhusak/vim-dadbod-ui", enabled = false },
 	{ "MagicDuck/grug-far.nvim", enabled = false },
 	{ "tpope/vim-dadbod", enabled = false },
+	{ "kristijanhusak/vim-dadbod-completion", enabled = false },
+	{ "folke/tokyonight.nvim", enabled = false },
 }
