5c3eb3bd679d39fd304bcad1d16880c4f5752276
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-07-22 09:37:37 +0200

    nvim: added sql support

--- common/nvim/.config/nvim/lua/plugins/disabled.lua
+++ common/nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,8 +1,5 @@
 return {
 	{ "folke/flash.nvim", enabled = false },
-	{ "kristijanhusak/vim-dadbod-ui", enabled = false },
 	{ "MagicDuck/grug-far.nvim", enabled = false },
-	{ "tpope/vim-dadbod", enabled = false },
-	{ "kristijanhusak/vim-dadbod-completion", enabled = false },
 	{ "folke/tokyonight.nvim", enabled = false },
 }
