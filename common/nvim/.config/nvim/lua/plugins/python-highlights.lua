6782b1b163601cdbbdffbc567cc88a012724bebb
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-07-22 09:52:50 +0200

    nvim: replaced ty with pyrefly

--- common/nvim/.config/nvim/lua/plugins/python-highlights.lua
+++ common/nvim/.config/nvim/lua/plugins/python-highlights.lua
@@ -1,20 +1,9 @@
 return {
-  -- ponytail: pyright semantic tokens override treesitter with incomplete theme
-  -- support => disable them, treesitter highlighting is cleaner.
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        pyright = {
-          capabilities = {
-            semanticTokens = false,
-          },
-        },
-        basedpyright = {
-          capabilities = {
-            semanticTokens = false,
-          },
-        },
+        pyrefly = {},
       },
     },
   },
