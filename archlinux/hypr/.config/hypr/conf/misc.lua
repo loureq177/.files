bc2d4bf9d13844100abd676c7c29c3f9808b463e
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-08-07 23:43:03 +0200

    hypr: disable adaptive sync (vrr)

--- archlinux/hypr/.config/hypr/conf/misc.lua
+++ archlinux/hypr/.config/hypr/conf/misc.lua
@@ -3,7 +3,7 @@ hl.config({
 		force_default_wallpaper = 0, -- Set to 0 or 1 to disable the anime mascot wallpapers
 		disable_hyprland_logo = true, -- If true disables the random hyprland logo / anime girl background. :(
 		focus_on_activate = true,
-		vrr = 2,
+		vrr = 0,
 	},
 	debug = { disable_scale_checks = true },
 })
