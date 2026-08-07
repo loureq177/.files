c1a6550131a3b4e502946593f9349398d557ed9f
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-08-07 09:25:29 +0200

    nvim: reenable tokyonight and drop python ruff conform config

--- common/nvim/.config/nvim/lua/plugins/disabled.lua
+++ common/nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,4 @@
 return {
 	{ "folke/flash.nvim", enabled = false },
 	{ "MagicDuck/grug-far.nvim", enabled = false },
-	{ "folke/tokyonight.nvim", enabled = false },
 }
--- common/nvim/.config/nvim/lua/plugins/python.lua
+++ common/nvim/.config/nvim/lua/plugins/python.lua
@@ -15,12 +15,5 @@ return {
 			},
 		},
 	},
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			formatters_by_ft = {
-				python = { "ruff_organize_imports", "ruff_format" },
-			},
-		},
-	},
 }
+
