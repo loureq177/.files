9efbac4824f0383a991bfd487a187538403fb581
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-08-09 13:27:53 +0200

    hypr: wake displays on key press and mouse move

--- archlinux/hypr/.config/hypr/conf/misc.lua
+++ archlinux/hypr/.config/hypr/conf/misc.lua
@@ -3,6 +3,8 @@ hl.config({
 		force_default_wallpaper = 0, -- Set to 0 or 1 to disable the anime mascot wallpapers
 		disable_hyprland_logo = true, -- If true disables the random hyprland logo / anime girl background. :(
 		focus_on_activate = true,
+		key_press_enables_dpms = true,
+		mouse_move_enables_dpms = true,
 		vrr = 0,
 	},
 	debug = { disable_scale_checks = true },
